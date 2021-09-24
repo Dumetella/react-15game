45af36a2d8aacb1c75bdb166a8beff9512c086d0
Dumetella <dumetella@outlook.com>
2021-09-24 07:53:13 +0300

    minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,26 @@ import Homepage from './pages/Homepage';
 import Takenpage from './pages/Takenpage';
 import styled from 'styled-components';
 
-interface AppProps {
-  className?: string;
-}
 
 
-function App(props: AppProps) {
+
+function App() {
 
   return (
-    <div className={props.className}>
-      <Router>
-        <div className="app">
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/taken-game" component={Takenpage} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/taken-game" component={Takenpage} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
-export default styled(App)`
-  margin: 0;
-  padding: 0;
-  font-family: 'Roboto', sans-serif;
-`;
+export default App;
 
 
 //<Route path="/dashboard" component={() => <Dashboard stuff={stuff} />} />  
